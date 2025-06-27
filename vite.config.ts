91205d63e80115729ddd4fba4868bf62ae4e3c18
Alvaro Felix <system.h16@gmail.com>
2025-06-26 20:39:14 -0500

    Configurar base path para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // ** AÑADE ESTA LÍNEA **
+      base: '/prueba0/', // Asegúrate de que 'prueba0' coincida EXACTAMENTE con el nombre de tu repositorio
+      // **********************
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
